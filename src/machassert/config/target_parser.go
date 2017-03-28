2137416b73aa8c193475d44fe045a4dbc2a9666d
Tom <twichyliquid64@ciphersink.net>
2017-03-28 21:09:19 +1100

    Separate normalisation from validation

--- src/machassert/config/target_parser.go
+++ src/machassert/config/target_parser.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"errors"
 	"io/ioutil"
 
 	"github.com/hashicorp/hcl"
@@ -19,10 +20,15 @@ func ParseTargetSchema(data []byte) (*MachineSpec, error) {
 		return nil, err
 	}
 
+	err = normalizeMachineSpec(&outSpec)
+	if err != nil {
+		return nil, err
+	}
 	err = validateMachineSpec(&outSpec)
 	if err != nil {
 		return nil, err
 	}
+
 	return &outSpec, nil
 }
 
@@ -36,11 +42,27 @@ func ParseTargetSpecFile(fpath string) (*MachineSpec, error) {
 	return ParseTargetSchema(d)
 }
 
-func validateMachineSpec(spec *MachineSpec) error {
+func normalizeMachineSpec(spec *MachineSpec) error {
 	for k := range spec.Machine {
 		for i := range spec.Machine[k].Auth {
 			if spec.Machine[k].Auth[i].Password != "" { //If password is set, set the auth kind to password
 				spec.Machine[k].Auth[i].Kind = AuthKindPassword
+			}
+		}
+	}
+	return nil
+}
+
+func validateMachineSpec(spec *MachineSpec) error {
+	for k := range spec.Machine {
+		for i := range spec.Machine[k].Auth {
+			switch spec.Machine[k].Auth[i].Kind {
+			case AuthKindPassword:
+				if spec.Machine[k].Auth[i].Password == "" {
+					return errors.New("Password must be specified for password authentication")
+				}
+			default:
+				return errors.New("Invalid machine auth type")
 			}
 		}
 	}
