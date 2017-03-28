822c5ef9ed2366b3892c1461888312d45719eeae
Tom <twichyliquid64@ciphersink.net>
2017-03-28 19:06:17 +1100

    Added print command

--- src/machassert/massert/main.go
+++ src/machassert/massert/main.go
@@ -5,13 +5,22 @@ import (
 	"fmt"
 	"machassert/config"
 	"os"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
 var targetsFilePathVar string
+var modeVar string
 
 func processFlags() {
 	flag.StringVar(&targetsFilePathVar, "targets", "", "Path to targets file")
 	flag.Parse()
+	modeVar = flag.Arg(0)
+
+	if modeVar == "" {
+		fmt.Printf("USAGE: %s <mode> <assertion files>\n", os.Args[0])
+		os.Exit(1)
+	}
 
 	if targetsFilePathVar != "" { //If it is empty, we use the current machine we are on
 		if _, err := os.Stat(targetsFilePathVar); err != nil && os.IsNotExist(err) {
@@ -21,10 +30,7 @@ func processFlags() {
 	}
 }
 
-func main() {
-	processFlags()
-	var targets *config.MachineSpec
-
+func getTargetSpec() (targets *config.MachineSpec) {
 	if targetsFilePathVar == "" { //default: current machine
 		targets = config.DefaultTarget()
 	} else {
@@ -34,5 +40,16 @@ func main() {
 			fmt.Printf("Err parsing targets: %s\n", err.Error())
 			os.Exit(1)
 		}
+	}
+	return targets
+}
+
+func main() {
+	processFlags()
+	targets := getTargetSpec()
+
+	switch modeVar {
+	case "print":
+		spew.Println(targets)
 	}
 }
